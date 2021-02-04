97da67216a9127d03ff4d00e5ae5d669011c8b6f
Ilya Kosmachev <kosmachev.ie@gmail.com>
2021-02-04 10:06:10 +0300

    добавленны оповещения через telegram

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,24 @@
 const User = require('../models/user');
+const TelegramBot = require('node-telegram-bot-api');
+const token = '1668615288:AAGoHVny4TGpvSwCyrjDVKjV0IXrGP3W-Zs';
+const bot = new TelegramBot(token, { polling: false });
+const chatId = '-530346437'
+
 
 module.exports.userData = (req, res, next) => {
   const { name, email, telephone } = req.body;
   User.create({ name, email, telephone})
-
-  .then((data) => res.send ({data: data}))
+  // .then((data) => {
+  //   res.send({data: data})
+  // })
+  .then((data) => {
+    let msg = {
+      'Имя: ': data.name,
+      'E-mail: ': data.email,
+      'Телефон: ': data.telephone,
+    }
+    return bot.sendMessage(chatId, JSON.stringify(msg))
+  })
   .catch((err) => {
     next(err);
   })
