e78412b5e6cdfae25a5a78d828cb8c36d3f6489a
Ilya Kosmachev <kosmachev.ie@gmail.com>
2021-02-05 01:01:08 +0300

    сообщение в tg теперь стилизовано

--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,9 @@ const TelegramBot = require('node-telegram-bot-api');
 const token = '1668615288:AAGoHVny4TGpvSwCyrjDVKjV0IXrGP3W-Zs';
 const bot = new TelegramBot(token, { polling: false });
 const chatId = '-530346437'
+const opt = {
+  parse_mode: 'HTML'
+}
 
 
 module.exports.userData = (req, res, next) => {
@@ -12,12 +15,9 @@ module.exports.userData = (req, res, next) => {
   //   res.send({data: data})
   // })
   .then((data) => {
-    let msg = {
-      'Имя: ': data.name,
-      'E-mail: ': data.email,
-      'Телефон: ': data.telephone,
-    }
-    return bot.sendMessage(chatId, JSON.stringify(msg))
+    let msg = `Новый клиент!\n  <b>Имя:</b> ${data.name}\n  <b>E-mail:</b> ${data.email}\n  <b>Тел.:</b> ${data.telephone}`
+    console.log(msg)
+    return bot.sendMessage(chatId, msg, opt)
   })
   .catch((err) => {
     next(err);
