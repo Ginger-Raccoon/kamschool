98fad4b62140a07bb81b00033de122f0cd966fc3
Ilya Kosmachev <kosmachev.ie@gmail.com>
2021-02-14 20:30:07 +0300

    added error output

--- controllers/user.js
+++ controllers/user.js
@@ -6,19 +6,22 @@ const chatId = '-530346437'
 const opt = {
   parse_mode: 'HTML'
 }
+const ValidErr = require('../errors/valid-err');
 
 
 module.exports.userData = (req, res, next) => {
   const { name, email, telephone } = req.body;
-  User.create({ name, email, telephone})
-  // .then((data) => {
-  //   res.send({data: data})
-  // })
+  User.create({ name, email, telephone })
   .then((data) => {
     let msg = `Новый клиент!\n  <b>Имя:</b> ${data.name}\n  <b>E-mail:</b> ${data.email}\n  <b>Тел.:</b> ${data.telephone}`
-    return bot.sendMessage(chatId, msg, opt).then(() =>{res.send('Данные успешно отправлены')}) .catch((err) => { res.send(err)});
+    return bot.sendMessage(chatId, msg, opt)
+      .then(() =>{res.send('Данные успешно отправлены')})
+      .catch((err) => { res.send({ message: 'Проблема с ботом'})});
   })
   .catch((err) => {
-    next(err);
+    if (err._message === 'user validation failed') {
+      res.send({ message: 'Введены неверные данные'});
+    }
+    next(err)
   })
 }
